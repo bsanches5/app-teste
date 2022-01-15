b30fef7272589083bdae6edb1be2e170d65ca8b1
Bruno <bsanches5@hotmail.com>
2022-01-15 01:27:59 -0300

    Consumindo API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
-function App() {
-    return ( <
-        div className = "App" >
-        <
-        header className = "App-header" >
-        <
-        img src = { logo }
-        className = "App-logo"
-        alt = "logo" / >
-        <
-        h1 > Olá Mundo < /h1>{" "} <
-        a className = "App-link"
-        href = "https://reactjs.org"
-        target = "_blank"
-        rel = "noopener noreferrer" >
-        Learn React { " " } <
-        /a>{" "} <
-        /header>{" "} <
-        /div>
-    );
+const App=() => {
+  const [allUsers, setAllUsers] = useState([]);
+  const [load] = useState("https://fakerapi.it/api/v1/users?_quantity=50");
+
+  useEffect(() => {
+    getAllUsers();
+  }, []);
+
+  const getAllUsers = async () => {
+    const res = await fetch(load);
+    const user = await res.json();
+
+    setAllUsers(user.data);
+  };
+
+  return (
+    <>
+    <h2>Lista de usuários</h2>
+    <div className="app-container">
+      {allUsers.map((values) => {
+        return (
+          <>
+            <div className="box">
+              <div className="content">
+                <h1>{values.image}</h1>
+                <h5>{values.firstname}</h5>
+                <h5>{values.lastname}</h5>
+                <p>{values.username}</p>
+                <p>{values.email}</p>
+                <h5>{values.website}</h5>
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </div>
+    </>
+  );
 }
 
 export default App;
